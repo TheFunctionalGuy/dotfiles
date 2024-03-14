f2c4045d094ca5346dc5744d6d1ad0da6a727187
Julian Vesper <jvesper95@gmail.com>
2024-03-14 01:55:21 +0100

    Add more missing configs

---
+++ dot_config/river/executable_launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Clear log
+: >"$1"
+
+# Wait for Waybar to start
+tail -f "$1" | grep -q "Bar configured"
+
+# Execute programs that need to run after Waybar
+keepassxc &
+nextcloud --background &
+ferdium &
