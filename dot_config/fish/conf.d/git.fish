236c71d4272e383c3f5b5a8cb1c47bd1622ce107
Julian Vesper <jvesper95@gmail.com>
2024-07-25 14:47:57 +0200

    Fish: Reorganize git abbreviations

--- dot_config/fish/conf.d/git.fish
+++ dot_config/fish/conf.d/git.fish
@@ -1,15 +1,26 @@
+# git basics
 abbr --add gC git clone
 abbr --add gco git checkout
 abbr --add gs git status
 abbr --add gf git fetch
 abbr --add gp git pull
 abbr --add gP git push
+abbr --add ga git add
+# git commit
 abbr --add gc git commit
 abbr --add gcm --set-cursor 'git commit -m "%'
+# git diff
 abbr --add gd git diff
 abbr --add gds git diff --staged
-abbr --add ga git add
+# git restore
 abbr --add gr git restore
 abbr --add grs git restore --staged
+# git (ref)log
 abbr --add gl git log
+abbr --add glr git reflog
+# git cleanup task
 abbr --add grpo git remote prune origin
+# git reset
+abbr --add gRs git reset --soft
+abbr --add gRm git reset --mixed
+abbr --add gRh git reset --hard
